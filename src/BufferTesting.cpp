082f1a04178c5462028fc46b07e1da21e941372d
DS-S <drs2559@rit.edu>
2023-12-06 00:31:06 -0500

    Initial creation of ring buffer template class and testing of class.

---
+++ src/BufferTesting.cpp
@@ -0,0 +1,71 @@
+#include "RingBuffer.hpp"
+#include <stdio.h>
+#include <iostream>
+//using namespace std;
+
+int main()
+{
+    int size = 2;
+    RingBuffer<uint8_t> channel1(size);
+    RingBuffer<uint8_t> channel2(size);
+    // channel1.reset();
+    // channel2.reset();
+
+    std::cout << std::boolalpha;
+
+    std::cout << "Initial State:" << std::endl;
+    std::cout << "\tChannel 1 empty " << channel1.empty() << "; Channel 2 empty " << channel2.empty() << "\r\n";
+    std::cout << "\tChannel 1 capacity " << channel1.capacity() << "; Channel 2 capacity " << channel2.capacity() << "\r\n";
+    std::cout << "\tChannel 1 size " << channel1.size() << "; Channel 2 size " << channel2.size() << "\r\n";
+    std::cout << "\tChannel 1 full " << channel1.full() << "; Channel 2 full " << channel2.full() << "\r\n";
+
+    std::cout << std::endl << "Adding Elements" << std::endl;
+    for (uint8_t i = 0; i < 3; i++)
+    {
+        channel1.put(i);
+        channel2.put(i);
+        printf("After iteration %d: \r\n", i);
+        std::cout << "\tChannel 1 empty " << channel1.empty() << "; Channel 2 empty " << channel2.empty() << "\r\n";
+        std::cout << "\tChannel 1 capacity " << channel1.capacity() << "; Channel 2 capacity " << channel2.capacity() << "\r\n";
+        std::cout << "\tChannel 1 size " << channel1.size() << "; Channel 2 size " << channel2.size() << "\r\n";
+        std::cout << "\tChannel 1 full " << channel1.full() << "; Channel 2 full " << channel2.full() << "\r\n";
+    }
+
+    std::cout << "After Adding: \r\n";
+    std::cout << "\tChannel 1 empty " << channel1.empty() << "; Channel 2 empty " << channel2.empty() << "\r\n";
+    std::cout << "\tChannel 1 capacity " << channel1.capacity() << "; Channel 2 capacity " << channel2.capacity() << "\r\n";
+    std::cout << "\tChannel 1 size " << channel1.size() << "; Channel 2 size " << channel2.size() << "\r\n";
+    std::cout << "\tChannel 1 full " << channel1.full() << "; Channel 2 full " << channel2.full() << "\r\n";
+
+    // std::cout << "Channel 1 full " << channel1.full() << "; Channel 2 full " << channel2.full() << "\r\n";
+    for (uint8_t i = 0; i < 2; i++)
+    {
+        auto d1 = channel1.get();
+        auto d2 = channel2.get();
+        if (d1)
+        {
+            printf("Channel 1 [%d] = %d\r\n",i,d1.value());
+            // std::cout << "Channel 1 [" << i << "] = " << d1.value() << "\r\n";
+        }
+        
+        if (d2)
+        {
+            printf("Channel 2 [%d] = %d\r\n",i,d2.value());
+            // std::cout << "Channel 2 [" << i << "] = " << d2.value() << "\r\n";
+        }
+
+        printf("After iteration %d: \r\n", i);
+        std::cout << "\tChannel 1 empty " << channel1.empty() << "; Channel 2 empty " << channel2.empty() << "\r\n";
+        std::cout << "\tChannel 1 capacity " << channel1.capacity() << "; Channel 2 capacity " << channel2.capacity() << "\r\n";
+        std::cout << "\tChannel 1 size " << channel1.size() << "; Channel 2 size " << channel2.size() << "\r\n";
+        std::cout << "\tChannel 1 full " << channel1.full() << "; Channel 2 full " << channel2.full() << "\r\n";
+    }
+    std::cout << "Channel 1 empty " << channel1.empty() << "; Channel 2 empty " << channel2.empty() << "\r\n";
+    std::cout << "Channel 1 size " << channel1.size() << "; Channel 2 size " << channel2.size() << "\r\n";
+    std::cout << "Channel 1 full " << channel1.full() << "; Channel 2 full " << channel2.full() << "\r\n";
+    channel1.reset();
+    channel2.reset();
+    std::cout << "Channel 1 empty " << channel1.empty() << "; Channel 2 empty " << channel2.empty() << "\r\n";
+    std::cout << "Channel 1 size " << channel1.size() << "; Channel 2 size " << channel2.size() << "\r\n";
+    std::cout << "Channel 1 full " << channel1.full() << "; Channel 2 full " << channel2.full() << "\r\n";
+}
