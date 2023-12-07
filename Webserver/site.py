f4ca890eca115dafc5b4a3315ce26391d363bc15
Promise <promiseomiponle@gmail.com>
2023-12-07 00:13:09 -0500

    Fixed the render_template calls to not have slashes for the page vies. Started work on having the received message actually forwarded to the requested client but could not figure             out how to correctly do so. get_ws_by_room() is just a placeholder for now and does not exist, but I can't find the substitute             for the "emit" function in Flask-SocketIO for the simple Flask-Sock library we are using.

--- Webserver/site.py
+++ Webserver/site.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-# from flask import SocketIO, emit
 from flask_sock import Sock
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
+app.config['SECRET_KEY'] = '61@sdL.Z=]tXnEZ'
+app.config['SOCK_SERVER_OPTIONS'] = {'ping_interval': 25}
 sock = Sock(app)
 
 # In-memory storage for user sessions
@@ -14,35 +14,37 @@ users = {}
 @app.route('/index')
 def index():
     if 'username' not in session:
-        return redirect(url_for('/login'))
+        return redirect(url_for('login'))
     return render_template('index.html', username=session['username'], user_id=session['user_id'])
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.form['username']
-    session['username'] = username
-    session['user_id'] = hash(username)  # Use a better method to generate user IDs
-    users[session['user_id']] = session['username']
-    return redirect(url_for('/index'))
-
-
-'''@app.route('/group')
-def group():
-    if 'username' not in session:
+    if request.method == 'POST':
+        username = request.form['username']
+        session['username'] = username
+        session['user_id'] = hash(username)  # Use a better method to generate user IDs
+        users[session['user_id']] = session['username']
         return redirect(url_for('index'))
-    return render_template('group.html', username=session['username'], user_id=session['user_id'])
-'''
+    return render_template('login.html')
 
 
-@sock.route('/ws')
-def echo(ws):
-    # emit('/ws', {'username': session['username'], '/ws': msg})
+@sock.route('/ws/<user_id>')
+def echo(ws, user_id):
     while True:
         data = ws.receive()
-        ws.send(data)
+        while True:
+            data = ws.receive()
+            ws.send(data)
+        '''Started work on having the received message actually forwarded to the requested client but could not figure
+            out how to correctly do so. get_ws_by_room() does not exist (thanks ChatGPT) but I can't find the substitute
+            for the "emit" function in Flask-SocketIO for the simple Flask-Sock library we are using.
+        # Iterate through connected clients and send the message individually
+        for client_id, client_username in users.items():
+            if client_id != user_id:  # Skip sending the message to the sender
+                connected_client_ws = sock.get_ws_by_room(str(client_id))
+                connected_client_ws.send(data)'''
 
 
 if __name__ == '__main__':
-    app.run()
-    # sock.run(app, debug=True)
+    app.run(debug=True)
